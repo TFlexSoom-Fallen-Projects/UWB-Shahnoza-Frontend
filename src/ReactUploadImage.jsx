69b1a566f1a375add5f95da43a58670cb234cc99
TFlexSoom <tchilbert@live.com>
2020-04-17 21:31:50 -0700

    Derposoft's Commit

---
+++ src/ReactUploadImage.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+const axios = require("axios");
+
+class ReactUploadImage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: props.url,
+            file: null,
+            filename: ""
+        };
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onChangeFile = this.onChangeFile.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('myimg', this.state.file);
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            },
+            body: {
+                'name': this.state.filename
+            }
+        };
+        axios.post(this.state.url, formData, config)
+            .then((res) => console.log("The file is successfully uploaded: " + res)).catch(err => {});
+    }
+
+    onChangeFile(e) {
+        this.setState({ file: e.target.files[0] });
+    }
+
+    onChangeName(e) {
+        this.setState({ filename: e.target.value });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <h1>File Upload</h1>
+                <input type="file" name="myimg" onChange={this.onChangeFile} />
+                <input type="text" name="imgname" onChange={this.onChangeName} />
+                <button type="submit">Upload</button>
+            </form>
+        )
+    }
+}
+
+export default ReactUploadImage
--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import CssBaseline from "@material-ui/core/CssBaseline";
-
 import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-
-import onSubmitFile from "./requestHandler.js";
+import ReactUploadImage from './ReactUploadImage.jsx';
 
 import "./baseStyles.css";
 
-var domainName = "localhost:3000";
-
-
-
-
-
+var domainName = "http://localhost:3000";
 
 export default function ShahnozaApplication() {
   return (
@@ -40,20 +32,9 @@ export function TitleAnchor(props){
 
 export function UploadForm(props){
   return (
-    <Box component="div" m={1}>
-      <h2 className="headline">Upload an Existing Photo</h2>
-      <form method="POST" action={domainName + "/upload"} className="app-form" onSubmit={onSubmitFile}>
-        <input type="file"/>
-        <button type="submit">Submit!</button>
-      </form>
-    </Box>
+    <ReactUploadImage url={domainName + "/upload"} />
   );
 }
 
 ReactDOM.render(<ShahnozaApplication />, document.getElementById("root"));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
