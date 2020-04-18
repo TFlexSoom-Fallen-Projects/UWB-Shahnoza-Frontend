9c13a3800c8b4b4c0db5f8696bbc61a6364196ab
TFlexSoom <tchilbert@live.com>
2020-04-17 18:29:31 -0700

    Application with an interface

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-ReactDOM.render(
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+import baseStyle from "./baseStyle.css";
+
+var domainName = "localhost:3000"
+
+export default function ShahnozaApplication() {
+  return (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+      <CssBaseline />
+      <baseStyle>
+        <Container maxWidth="lg" id="main-container">
+          <WelcomeTitle text="Welcome to Shahnoza"/>
+          <TitleAnchor text="Take a Photo!" link="upload.html"/>
+          <UploadForm />
+        </Container>
+      </baseStyle>
+  </React.StrictMode>
+  );
+}
+
+export function WelcomeTitle(props){
+  return <h1 className="welcome-title">{props.text}</h1>;
+}
+
+export function TitleAnchor(props){
+  return <a className="title-anchor" href={props.link}>{props.text}</a>;
+}
+
+export function UploadForm(props){
+  return (
+    <Box component="div" m={1}>
+      <h2 className="headline">Upload an Existing Photo</h2>
+      <form method="POST" action={domainName + "/upload"}>
+        <input type="file" />
+      </form>
+    </Box>
+  );
+}
+
+ReactDOM.render(<ShahnozaApplication />, document.getElementById("root"));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
